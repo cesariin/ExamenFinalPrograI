14bef222f04dbfd3f9d9579b51f88e1b35aa7764
cesariin <105024647+cesariin@users.noreply.github.com>
2023-06-10 10:59:35 -0600

    Add files via upload

---
+++ compartir.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examen_final_progra
+{
+    public class compartir
+    {
+        public const string ONNECTION_STRING = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\cesar\\Desktop\\examen final progra\\examen final progra\\Producto.mdf\";Integrated Security=True";
+    }
+}
